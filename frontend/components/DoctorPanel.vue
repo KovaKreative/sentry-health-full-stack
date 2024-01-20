f554cd1522cd4a07de51caf878d2bd2f06835dd5
KovaKreative <eric.kovalevskyy@gmail.com>
2024-01-19 20:14:44 -0500

    Implement Doctor panel

--- frontend/components/DoctorPanel.vue
+++ frontend/components/DoctorPanel.vue
@@ -1,10 +1,34 @@
 <script setup>
 
+import Appointments from './Appointments.vue';
+
+import { getDoctors, getDoctorData } from '~/helpers/apiCalls';
+
+const doctors = ref([]);
+const doctor = ref(null);
+const appointments = ref([]);
+
+onMounted(() => {
+  getDoctors(doctors.value);
+});
+
+watch(doctor, () => {
+  getDoctorData(doctor.value.id, appointments);
+  console.log(appointments);
+});
+
 </script>
 
 <template>
   <div>
-    Doctor Panel
+    <h1>Doctor Panel</h1>
+    <form v-if="doctors.length > 0" class="patient-select">
+      <label for="doctor">Doctor:</label>
+      <select name="doctor" v-model="doctor">
+        <option v-for="item in doctors" :value="item">Dr. {{ item.name }}</option>
+      </select>
+    </form>
+    <Appointments v-if="appointments.length > 0" :appointments="appointments" />
   </div>
 </template>
 
